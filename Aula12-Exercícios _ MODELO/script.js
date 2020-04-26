16034ce6129a980de8d446fa6cd84e395c61626f
leonardo cesar <leocesar1977@Gmail.com>
2020-04-26 19:17:48 -0300

    Aula12 Exercícios - Continuação

--- Aula12-Exercícios _ MODELO/script.js
+++ Aula12-Exercícios _ MODELO/script.js
@@ -5,4 +5,17 @@ var img = window.document.getElementById('imagem')
 var data = new Date()
 var hora = data.getHours()
 msg.innerHTML = `Agora são ${hora} horas`
+if (hora > 0 && hora < 12) {
+    img.src = 'manha.png'
+    document.body.style.background = '#e2cd9f'
+    //BOM DIA
+} else if (hora > 12 && hora < 18) {
+        img.src = 'tarde.png'
+        document.body.style.background = '#b9846f'
+        //BOA TARDE
+    } else {
+        img.src = 'noite.png'
+        document.body.style.background = '#515154'
+        //BOA NOITE
+    }
 }
