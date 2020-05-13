99750230ce779d701dbcf4059eb66b222e2e20df
leonardo cesar <leocesar1977@Gmail.com>
2020-05-13 09:42:32 -0300

    Aula 15

---
+++ Aula15/ambiente.js
@@ -0,0 +1,14 @@
+let num = [5, 7, 9, 3]
+
+num.sort()
+
+num[4] = 11 
+
+num.push(13)//acrescenta um elemento na última posição do array
+
+console.log(`O vetor num é o ${num}`)
+
+let tam = num.length//tamanho do array
+
+console.log(`O vetor num tem ${num} posições.`)
+
