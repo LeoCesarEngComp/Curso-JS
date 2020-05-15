89764acf0429cf05f9fc9ea8d97ce9405fec13ac
leonardo cesar <leocesar1977@Gmail.com>
2020-05-14 22:40:40 -0300

    Aula 16 - Exercícios

--- Aula16 - Exercício/script.js
+++ Aula16 - Exercício/script.js
@@ -1,9 +1,31 @@
-function tabuada() {
-    let num = document.getElementById('txtn')
-    let tab = document.getElementById('seltab')
+let num = document.querySelector('input#fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
 
-    if (num.value.lenght == 0) {
-        window.alert('ERRO')
+function isNumber(n) {
+    if (Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
     }
-    //let n = Number(nun.value)
 }
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function adicionar() {
+    if (isNumber(num.value) && !inList(num.value, valores))  {
+        
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
+    }
+    
+    
+    
+    }
