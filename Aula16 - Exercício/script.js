4ac1572fcd65946d3be246cff480224d26999ee4
leonardo cesar <leocesar1977@Gmail.com>
2020-05-14 21:37:04 -0300

    Aula 16 - Exercícios

---
+++ Aula16 - Exercício/script.js
@@ -0,0 +1,9 @@
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+
+    if (num.value.lenght == 0) {
+        window.alert('ERRO')
+    }
+    //let n = Number(nun.value)
+}
